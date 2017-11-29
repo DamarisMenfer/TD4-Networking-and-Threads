3cb3f584f88b6abf98f193c20c82e4ba2b331fae
mendesfe <damaris.menfer@gmail.com>
2017-11-29 09:39:54 +0100

    UDP implemented.

--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
@@ -1,8 +1,48 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class UDPMessageReceiverService implements MessageReceiverService {
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+        DatagramSocket sock = null;
+
+        try
+        {
+            //1. creating a server socket, parameter is local port number
+            sock = new DatagramSocket(port);
+
+            //buffer to receive incoming data
+            byte[] buffer = new byte[65536];
+            DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+
+            //2. Wait for an incoming data
+            echo("Server socket created. Waiting for incoming data...");
+
+            //communication loop
+
+                sock.receive(incoming);
+                byte[] data = incoming.getData();
+                String s = new String(data, 0, incoming.getLength());
+
+                //echo the details of incoming data - client ip : client port - client message
+                echo(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
+
+                s = "OK : " + s;
+                DatagramPacket dp = new DatagramPacket(s.getBytes() , s.getBytes().length , incoming.getAddress() , incoming.getPort());
+                sock.send(dp);
+
+        }
+
+        catch(IOException e) {
+            System.err.println("IOException " + e);
+        }
+    }
+
+    public static void echo(String msg)
+    {
+        System.out.println(msg);
     }
 }
--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageSenderService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageSenderService.java
@@ -1,8 +1,35 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+
 public class UDPMessageSenderService implements MessageSenderService {
     @Override
     public void sendMessageOn(String ipAddress, int port, String message) throws Exception {
-        throw new UnsupportedOperationException();
+        DatagramSocket sock = null;
+
+        BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
+
+        try
+        {
+            sock = new DatagramSocket();
+
+            InetAddress host = InetAddress.getByName("localhost");
+
+            byte[] b = message.getBytes();
+
+            DatagramPacket  dp = new DatagramPacket(b , b.length , host , port);
+            sock.send(dp);
+
+        }
+
+        catch(IOException e)
+        {
+            throw new UnsupportedOperationException();
+        }
+
     }
 }
